aad65109e023a57b697dc0dcbd2ecf8cdd4fcd9f
alexIGit <toevilhell@gmail.com>
2023-11-21 22:19:23 +0300

    django dev server using HTTPS protocol

--- djangoapps/README.md
+++ djangoapps/README.md
@@ -1,1 +1,14 @@
 вместо pip исп-ся poetry
+
+# dev сервер на HTTPS
+mkcert
+mkcert -install
+mkcert localhost
+rename certificates to .key .crt
+
+-- python packages
+django_extensions
+werkzeug
+pyopenssl
+
+python manage.py runserver_plus 0.0.0.0:8081 --cert-file srv/localhost.crt
--- djangoapps/pro/settings.py
+++ djangoapps/pro/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "middleware1"
+    "middleware1",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+SECURE_SSL_REDIRECT = True
